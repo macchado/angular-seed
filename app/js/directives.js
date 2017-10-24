08b6d61c92808dcc3d6d67b81ac2d4a8832f4188
antonio machado <macchado@users.noreply.github.com>
2017-10-24 10:42:46 -0200

    updating indentation

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,8 +4,9 @@
 
 
 angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
+  directive('appVersion', 
+            ['version', function(version) {
+                          return function(scope, elm, attrs) {
+                          elm.text(version);
+                        };
   }]);
